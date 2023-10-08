b71104f56cf97e588dac657ca7e241c254bd8807
safdar-azeem <safdarazeem00@gmail.com>
2023-10-08 18:26:46 +0500

    Delete index.ts

--- functions/graphql/form/index.ts
+++
@@ -1,6 +0,0 @@
-import { types } from './types'
-import { resolvers } from './resolver'
-import { mutations } from './mutations'
-import { queries } from './queries'
-
-export const Form = { types, resolvers, mutations, queries }
